83d29d43282c4333c358e37ca7c0f22b5023c35d
Edwin Chajon <junchajo@gmail.com>
2019-10-29 13:44:04 -0700

    load, drag/drop

--- js/index.js
+++ js/index.js
@@ -43,3 +43,65 @@ function zoom(event) {
     let scale = 1;
     const el = document.querySelector('img');
     el.onwheel = zoom;
+
+    //load
+window.addEventListener('load', (event) => {
+    function random_bg_color() {
+        var x = Math.floor(Math.random() * 256);
+        var y = Math.floor(Math.random() * 256);
+        var z = Math.floor(Math.random() * 256);
+        var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+     console.log(bgColor);
+      
+        document.body.style.background = bgColor;
+        }
+    
+    random_bg_color();
+    });
+
+// drag/drop
+
+
+var dragged;
+
+document.addEventListener("drag", function(event) {
+
+}, false);
+
+document.addEventListener("dragstart", function(event) {
+
+  dragged = event.target;
+  event.target.style.opacity = .5;
+}, false);
+
+document.addEventListener("dragend", function(event) {
+  event.target.style.opacity = "";
+}, false);
+
+document.addEventListener("dragover", function(event) {
+  event.preventDefault();
+}, false);
+
+document.addEventListener("dragenter", function(event) {
+if (event.target.className == "img-content") {
+    event.target.style.background = "purple";
+  }
+
+}, false);
+
+document.addEventListener("dragleave", function(event) {
+if (event.target.className == "img-content") {
+    event.target.style.background = "";
+  }
+
+}, false);
+
+document.addEventListener("drop", function(event) {
+event.preventDefault();
+  if (event.target.className == "img-content") {
+    event.target.style.background = "";
+    dragged.parentNode.removeChild( dragged );
+    event.target.appendChild( dragged );
+  }
+}, false);
+
